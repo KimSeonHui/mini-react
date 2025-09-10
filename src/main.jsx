c353dab69952839b61ed7fb72c0a6581fdabf7d1
KimSeonHui <zero9657@naver.com>
2025-09-10 21:24:50 +0900

    createElement type이 function 케이스 추가, 파일명 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,33 @@
-const app = document.getElementById('app');
-app.innerHTML = `
+const MyName = () => {
+  return <span>I'm sunny</span>;
+};
+
+// jsx 테스트
+console.log(
   <div>
-    <h1>hello</h1>
+    1<h1>hello</h1>
     <p>
       paragrahp<span>span tag</span>
+      <MyName />
     </p>
-  </div>
-`;
+  </div>,
+);
+
+// createElement 테스트
+const props = {
+  className: 'container',
+  id: 'app',
+};
+
+const Component = ({ props }) => {
+  return (
+    <div {...props} key="1">
+      <p>hello</p>
+      <span>span tag</span>
+    </div>
+  );
+};
+
+console.log(<Component {...props} />);
 
 // TODO : render
